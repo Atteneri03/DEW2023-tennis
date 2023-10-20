5e35874228eba79e4fe05d473528277e621c412d
Atteneri Estévez <atteeste2003@gmail.com>
2023-10-20 20:16:15 +0100

    Players y test y Match inacabado

--- Player.js
+++ Player.js
@@ -1,12 +1,16 @@
 class Player {
     name;
+    ranking;
 
-    constructor(name){
+    constructor(name,ranking){
         this.name = name;
+        this.ranking = ranking;
     }
 }
 
+module.exports = Player;
+
 //Esto son pruebas (ELIMINAR A PARTIR DE AQUI)
 
-let player1 = new Player("Juan");
-console.log(player1);
+// let player1 = new Player("Juan");
+// console.log(player1);
